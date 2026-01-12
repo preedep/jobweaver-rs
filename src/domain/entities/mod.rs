d5c1d854f519ff0a6b08e0b13ff82600164f1770
Preedee@Digital.Dev <nickmsft@mac.com>
2026-01-12 16:56:05 +0700

    Expand entity exports to include `FolderType` and `DoAction`

--- src/domain/entities/mod.rs
+++ src/domain/entities/mod.rs
@@ -6,8 +6,8 @@ pub mod resource;
 pub mod scheduling;
 
 pub use job::Job;
-pub use folder::Folder;
+pub use folder::{Folder, FolderType};
 pub use dependency::{Dependency, DependencyType};
-pub use condition::{Condition, ConditionType, OnCondition};
+pub use condition::{Condition, ConditionType, OnCondition, DoAction};
 pub use resource::{ControlResource, QuantitativeResource};
 pub use scheduling::SchedulingInfo;
